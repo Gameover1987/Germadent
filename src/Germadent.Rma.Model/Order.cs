4f69510f524f3aaa3a82fb831f2c38e9edb0835e
Alexey <alfer.nsk@gmail.com>
2019-11-29 19:25:12 +0700

    RMA-3 Наполнение печатного шаблона для ЗТЛ, правка шаблона ФЦ

--- src/Germadent.Rma.Model/Order.cs
+++ src/Germadent.Rma.Model/Order.cs
@@ -4,7 +4,7 @@ namespace Germadent.Rma.Model
 {
     public class Order
     {
-        public int Id { get; set; }
+        public int OrderId { get; set; }
 
         public int Number { get; set; }
 
@@ -17,16 +17,22 @@ namespace Germadent.Rma.Model
         public string Customer { get; set; }
 
         public string Patient { get; set; }
+        public int PatientAge { get; set; }
+        public bool PatientGender { get; set; }
+
 
         public string Employee { get; set; }
 
         public string Material { get; set; }
         public string ResponsiblePerson { get; set; }
         public string RespPersPhone { get; set; }
-        public string AdditionalInfo { get; set; }
         public string CarcassColor { get; set; }
         public string ImplantSystem { get; set; }
         public string IndividualAbutmentProcessing { get; set; }
         public string WorkDescription { get; set; }
+        public string TypeOfWork { get; set; }
+        public DateTime DateOfCompletion { get; set; }
+        public DateTime FittingDate { get; set; }
+        public string ColorAndFeatures { get; set; }
     }
 }
