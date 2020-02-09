4430083ebfdd3ea65aa874c99868f16a534b6e02
Alexey <alfer.nsk@gmail.com>
2020-02-09 20:58:08 +0700

    Заготовки для прозрачностей

---
+++ src/Germadent.DataAccessService/Entities/TransparencesEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Germadent.DataAccessService.Entities
+{
+    public class TransparencesEntity
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public int TransparenceId { get; set; }
+        /// <summary>
+        /// Название
+        /// </summary>
+        public string TransparenceName { get; set; }
+    }
+}
