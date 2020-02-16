70480cc7977539ee231a28e5436109890f7acb46
Gameover1987 <slnec@yandex.ru>
2020-02-17 00:18:32 +0700

    Начал делать добавление файлов к заказ наряду

--- src/Germadent.Rma.App/Germadent.Rma.App/ServiceClient/RmaOperations.cs
+++ src/Germadent.Rma.App/Germadent.Rma.App/ServiceClient/RmaOperations.cs
@@ -1,21 +1,26 @@
 ﻿using System;
+using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using Germadent.Common.Extensions;
+using Germadent.Common.FileSystem;
 using Germadent.Rma.App.Configuration;
 using Germadent.Rma.Model;
+using RestSharp;
 
 namespace Germadent.Rma.App.ServiceClient
 {
     public class RmaOperations : IRmaOperations
     {
         private readonly IConfiguration _configuration;
+        private readonly IFileManager _fileManager;
         private readonly HttpClient _client;
 
-        public RmaOperations(IConfiguration configuration)
+        public RmaOperations(IConfiguration configuration, IFileManager fileManager)
         {
             _configuration = configuration;
+            _fileManager = fileManager;
 
             _client = new HttpClient();
             _client.BaseAddress = new Uri(_configuration.DataServiceUrl);
@@ -29,7 +34,7 @@ namespace Germadent.Rma.App.ServiceClient
                 ordersFilter = new OrdersFilter();
 
             var content = new StringContent(ordersFilter.SerializeToJson(), Encoding.UTF8, "application/json");
-
+            
             var apiUrl = _configuration.DataServiceUrl + "/api/Rma/getOrdersByFilter";
             using (var response = _client.PostAsync(apiUrl, content).Result)
             {
@@ -80,14 +85,31 @@ namespace Germadent.Rma.App.ServiceClient
 
         public OrderDto AddOrder(OrderDto order)
         {
-            var content = new StringContent(order.SerializeToJson(), Encoding.UTF8, "application/json");
+            var client = new RestClient();
+            IRestRequest restRequest = new RestRequest(_configuration.DataServiceUrl+"/api/Rma/addOrder", Method.POST);
 
-            var apiUrl = _configuration.DataServiceUrl + "/api/Rma/addOrder";
-            using (var response = _client.PostAsync(apiUrl, content).Result)
-            {
-                var result = response.Content.ReadAsStringAsync().Result;
-                return result.DeserializeFromJson<OrderDto>();
-            }
+            restRequest.AlwaysMultipartFormData = true;
+            restRequest.RequestFormat = DataFormat.Json;
+
+            // I just add File To Request
+            restRequest.AddFile("OrderData", _fileManager.ReadAllBytes(order.DataFileName), "OrderData");
+
+            // Then Add Json Object
+            restRequest.AddBody(order);
+
+            client.Execute(restRequest, Method.POST);
+
+            return order;
+
+
+            //var content = new StringContent(order.SerializeToJson(), Encoding.UTF8, "application/json");
+
+            //var apiUrl = _configuration.DataServiceUrl + "/api/Rma/addOrder";
+            //using (var response = _client.PostAsync(apiUrl, content).Result)
+            //{
+            //    var result = response.Content.ReadAsStringAsync().Result;
+            //    return result.DeserializeFromJson<OrderDto>();
+            //}
         }
 
         public OrderDto UpdateOrder(OrderDto order)
@@ -100,11 +122,6 @@ namespace Germadent.Rma.App.ServiceClient
                 var result = response.Content.ReadAsStringAsync().Result;
                 return result.DeserializeFromJson<OrderDto>();
             }
-        }
-
-        public void UploadStlFile(int id, byte[] stlFile)
-        {
-            throw new NotImplementedException();
         }
 
         public TransparencesDto[] GetTransparences()
