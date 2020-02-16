c896b41e9ed2d2a9d187c3d71d7c28fccb1bed27
Alexey <alfer.nsk@gmail.com>
2020-02-16 22:24:08 +0700

    Добавления, исправления по оснастке для ФЦ

---
+++ db/Germadent/Germadent.DB/dbo/Functions/GetAdditionalEquipment.sql
@@ -0,0 +1,17 @@
+﻿-- =============================================
+-- Author:		Alexey Kolosenok
+-- Create date: 16.02.2020
+-- Description:	Возвращает набор идентификаторов для заказ-наряда и оснастки, а также количество оснастки 
+-- =============================================
+CREATE FUNCTION [dbo].[GetAdditionalEquipment]
+(	
+	@workOrderId int = NULL
+)
+RETURNS TABLE 
+AS
+RETURN 
+(
+	SELECT *
+	FROM AdditionalEquipment
+	WHERE WorkOrderID = @workOrderId
+)
