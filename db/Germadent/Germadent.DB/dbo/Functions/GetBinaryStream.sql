8a34d2315fe9d87f070b9be469ddd31f8c099c73
Alexey <alfer.nsk@gmail.com>
2020-02-20 13:32:27 +0700

    Добавление функционала по файловым таблицам

---
+++ db/Germadent/Germadent.DB/dbo/Functions/GetBinaryStream.sql
@@ -0,0 +1,26 @@
+﻿-- =============================================
+-- Author:		Алексей Колосенок
+-- Create date: 19.02.2020
+-- Description:	Возвращает двоичный поток для заданного stream_id
+-- =============================================
+CREATE FUNCTION GetBinaryStream
+(
+	@streamId uniqueidentifier
+)
+RETURNS varbinary(max)
+AS
+BEGIN
+	DECLARE @fileStream varbinary(max)
+
+	SELECT @fileStream = file_stream
+	FROM StlAndPhotos
+	WHERE stream_id = @streamId
+
+	RETURN @fileStream
+
+END
+GO
+GRANT EXECUTE
+    ON OBJECT::[dbo].[GetBinaryStream] TO [gdl_user]
+    AS [dbo];
+
--- db/Germadent/Germadent.DB/dbo/Stored Procedures/AddLinkWO_FileStream.sql
+++ db/Germadent/Germadent.DB/dbo/Stored Procedures/AddLinkWO_FileStream.sql
@@ -3,10 +3,10 @@
 -- Create date: 08.02.2020
 -- Description:	Добавление связи заказ-наряда и файлового хранилища
 -- =============================================
-CREATE PROCEDURE AddLinkWO_FileStream 
+CREATE PROCEDURE [dbo].[AddLinkWO_FileStream] 
 	
 	@fileName nvarchar(70),
-	@creationTime datetime,
+	@creationTime datetimeoffset,
 	@workOrderId int
 	
 AS
@@ -14,12 +14,26 @@ BEGIN
 	
 	SET NOCOUNT ON;
 
-	INSERT INTO FileStreams
+	DECLARE @streamId uniqueidentifier
+
+	-- Получение id файлового потока по имени файла и времени его создания
+	SET @streamId = (SELECT stream_id FROM StlAndPhotos	WHERE name = @fileName	AND creation_time = @creationTime)
+
+	-- Проверка, если такая связь уже есть
+	IF EXISTS (SELECT stream_id FROM LinksFileStreams WHERE stream_id = @streamId AND WorkOrderID = @workOrderId)
+	BEGIN
+		RETURN
+	END
+
+	-- Собственно вставка
+	INSERT INTO LinksFileStreams
 	(WorkOrderID, stream_id)
 	VALUES
-    (@workOrderId, (SELECT stream_id 
-					FROM StlAndPhotos	
-					WHERE name = @fileName	
-						AND creation_time = @creationTime))
+    (@workOrderId, @streamId)
 
 END
+GO
+GRANT EXECUTE
+    ON OBJECT::[dbo].[AddLinkWO_FileStream] TO [gdl_user]
+    AS [dbo];
+
