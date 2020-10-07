5b61c869428c99d546f878b08ae2369032f8bc34
Gameover1987 <slnec@yandex.ru>
2020-10-07 13:50:00 +0700

    Добавил контрол для прайслиста

---
+++ src/Germadent.Rma.App/Germadent.Rma.App/ViewModels/Pricing/PriceListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Germadent.UI.ViewModels;
+
+namespace Germadent.Rma.App.ViewModels.Pricing
+{
+    public class PriceListViewModel : ViewModelBase
+    {
+        
+    }
+}
