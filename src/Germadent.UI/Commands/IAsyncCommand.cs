ac6dfb37e263d48fd3fba3e2a3d293ce7f7659fe
icapgameover <icap.gameover@gmail.com>
2020-12-07 02:36:52 +0700

    Добавил асинхронную команду

---
+++ src/Germadent.UI/Commands/IAsyncCommand.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Germadent.UI.Commands
+{
+    /// <summary>
+    /// Интерфейс асинхронной команды
+    /// </summary>
+    public interface IAsyncCommand : ICommand
+    {
+        Task ExecuteAsync(object parameter);
+    }
+}
