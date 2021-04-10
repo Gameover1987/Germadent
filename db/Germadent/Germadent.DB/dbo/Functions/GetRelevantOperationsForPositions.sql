8687661390301b6e3fe42c6902e8c83418b29712
alfernsk <alfer.nsk@gmail.com>
2021-04-10 15:31:27 +0700

    Для РМС

---
+++ db/Germadent/Germadent.DB/dbo/Functions/GetRelevantOperationsForPositions.sql
@@ -0,0 +1,19 @@
+﻿-- =============================================
+-- Author:		Алексей Колосенок
+-- Create date: 10.04.2021
+-- Description:	Возвращает набор технологических операций, доступных сотруднику согласно его должности
+-- =============================================
+CREATE FUNCTION dbo.GetRelevantOperationsForPositions
+(	
+	@userId int	
+)
+RETURNS TABLE 
+AS
+RETURN 
+(
+	SELECT t.TechnologyOperationID, t.TechnologyOperationName
+	FROM dbo.Users u 
+		INNER JOIN dbo.EmployeePositionsCombination epc ON u.UserID = epc.EmployeeID
+		INNER JOIN dbo.EmployeePositions ep ON epc.EmployeePositionID = ep.EmployeePositionID
+		INNER JOIN dbo.TechnologyOperations t ON ep.EmployeePositionID = t.EmployeePositionID
+)
---
+++ db/Germadent/Germadent.DB/dbo/Tables/WorkList.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[WorkList] (
+    [WorkOrderID]           INT      NOT NULL,
+    [ProductID]             INT      NOT NULL,
+    [TechnologyOperationID] INT      NOT NULL,
+    [EmployeeID]            INT      NOT NULL,
+    [OperationCost]         MONEY    NULL,
+    [Started]               DATETIME NULL,
+    [Ended]                 DATETIME NULL,
+    [IsChecked]             BIT      NULL,
+    CONSTRAINT [FK_WorkList_Products] FOREIGN KEY ([ProductID]) REFERENCES [dbo].[Products] ([ProductID]),
+    CONSTRAINT [FK_WorkList_TechnologyOperations] FOREIGN KEY ([TechnologyOperationID]) REFERENCES [dbo].[TechnologyOperations] ([TechnologyOperationID]),
+    CONSTRAINT [FK_WorkList_Users] FOREIGN KEY ([EmployeeID]) REFERENCES [dbo].[Users] ([UserID]),
+    CONSTRAINT [FK_WorkList_WorkOrder] FOREIGN KEY ([WorkOrderID]) REFERENCES [dbo].[WorkOrder] ([WorkOrderID]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_WorkOperations]
+    ON [dbo].[WorkList]([WorkOrderID] ASC, [TechnologyOperationID] ASC);
+
