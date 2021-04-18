b351c5a0d18084948065f0b1f630e68a720ab642
alfernsk <alfer.nsk@gmail.com>
2021-04-18 21:55:39 +0700

    Мелкие правки в БД

--- db/Germadent/Germadent.DB/dbo/Tables/TechnologyOperations.sql
+++ db/Germadent/Germadent.DB/dbo/Tables/TechnologyOperations.sql
@@ -1,11 +1,14 @@
 ﻿CREATE TABLE [dbo].[TechnologyOperations] (
-    [TechnologyOperationID]   INT            IDENTITY (1, 1) NOT NULL,
-    [TechnologyOperationName] NVARCHAR (250) NOT NULL,
-    [EmployeePositionID]      INT            NOT NULL,
-    [GroupName]               NVARCHAR (50)  NOT NULL,
+    [TechnologyOperationID]       INT            IDENTITY (1, 1) NOT NULL,
+    [TechnologyOperationUserCode] NVARCHAR (20)  NULL,
+    [TechnologyOperationName]     NVARCHAR (250) NOT NULL,
+    [EmployeePositionID]          INT            NOT NULL,
+    [GroupName]                   NVARCHAR (50)  NULL,
     CONSTRAINT [PK_Works] PRIMARY KEY CLUSTERED ([TechnologyOperationID] ASC),
     CONSTRAINT [FK_TechnologyOperations_EmployeePositions] FOREIGN KEY ([EmployeePositionID]) REFERENCES [dbo].[EmployeePositions] ([EmployeePositionID])
 );
+
+
 
 
 
