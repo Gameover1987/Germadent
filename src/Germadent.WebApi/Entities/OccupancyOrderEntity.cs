9b966dd5f7c4e85a7471282d2915d62d4883e8f5
alfernsk <alfer.nsk@gmail.com>
2021-04-20 15:17:09 +0700

    Кое-какие сущности

---
+++ src/Germadent.WebApi/Entities/OccupancyOrderEntity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Germadent.Rma.Model;
+
+namespace Germadent.WebApi.Entities
+{
+    public class OccupancyOrderEntity
+    {
+        /// <summary>
+        /// Идентификатор заказ-наряда
+        /// </summary>
+        public int WorkOrderId { get; set; }
+        
+        /// <summary>
+        /// Номер заказ-наряда
+        /// </summary>
+        public string DocNumber { get; set; }
+        
+        /// <summary>
+        /// Дата/время открытия заказ-наряда
+        /// </summary>
+        public DateTime OccupancyDateTime { get; set; }
+
+        /// <summary>
+        /// Идентификатор пользователя
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Фамилия, инициалы пользователя
+        /// </summary>
+        public string UserFullName { get; set; }
+    }
+}
