20bfdbc03058b143faf17f5dd3bf739f1234bdb3
alfernsk <alfer.nsk@gmail.com>
2021-04-21 17:48:49 +0700

    Подготовка сущностей в проекте

---
+++ src/Germadent.WebApi/Entities/RateEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Germadent.WebApi.Entities
+{
+    public class RateEntity
+    {
+        /// <summary>
+        /// Идентификатор технологической операции
+        /// </summary>
+        public int TechnologyOperationId { get; set; }
+
+        /// <summary>
+        /// Квалификационный разряд
+        /// </summary>
+        public int QualifyingRank { get; set; }
+        
+        /// <summary>
+        /// Расценка
+        /// </summary>
+        public decimal Rate { get; set; }
+        
+        /// <summary>
+        /// Дата начала
+        /// </summary>
+        public DateTime DateBeginning { get; set; }
+        
+        /// <summary>
+        /// Дата окончания
+        /// </summary>
+        public DateTime DateEnd { get; set; }
+    }
+}
