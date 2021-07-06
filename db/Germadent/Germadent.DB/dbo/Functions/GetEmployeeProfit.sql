b006dc59ef16c32d28ad2d687ea65cd604e044e3
alfernsk <alfer.nsk@gmail.com>
2021-07-06 16:34:46 +0700

    Уточнённый отбор технологических операций и заказ-нарядов в БД

---
+++ db/Germadent/Germadent.DB/dbo/Functions/GetEmployeeProfit.sql
@@ -0,0 +1,29 @@
+﻿-- =============================================
+-- Author:		Alexey Kolosenok
+-- Create date: 30.06.2021
+-- Description:	<Description,,>
+-- =============================================
+CREATE FUNCTION GetEmployeeProfit
+(	
+	@userId int,
+	@dateFrom datetime,
+	@dateTo datetime
+)
+RETURNS TABLE 
+AS
+RETURN 
+(
+	WITH closedStatuses (WorkOrderID) AS
+	(SELECT WorkOrderID
+	FROM dbo.StatusList
+	WHERE Status > 89
+		AND StatusChangeDateTime BETWEEN ISNULL(@dateFrom, '17530101') AND ISNULL(@dateTo, '99991231'))
+
+	SELECT tc.WorkOrderID, wo.DocNumber, SUM(tc.Price) Proceeds, SUM(tc.Price) * 0.3 Profit
+	FROM dbo.ToothCard tc 
+		INNER JOIN dbo.WorkOrder wo ON tc.WorkOrderID = wo.WorkOrderID
+		INNER JOIN dbo.WorkList wl ON tc.WorkOrderID = wl.WorkOrderID AND tc.ProductID = wl.ProductID
+		INNER JOIN closedStatuses ON tc.WorkOrderID = closedStatuses.WorkOrderID
+	WHERE wl.EmployeeIDStarted = @userId
+	GROUP BY tc.WorkOrderID, wo.DocNumber
+)
---
+++ db/Germadent/Germadent.DB/dbo/Stored Procedures/GetRelevantWorkOrdersList.sql
@@ -0,0 +1,71 @@
+﻿-- =============================================
+-- Author:		Alexey Kolosenok
+-- Create date: 06.07.2021
+-- Description:	Возвращает список заказ-нарядов по заданным критериям отбора в зависимости от должности сотрудника
+-- =============================================
+CREATE PROCEDURE [dbo].[GetRelevantWorkOrdersList]
+(	
+	@branchTypeID int = NULL
+	, @branchType nvarchar(30) = NULL
+	, @workorderID int = NULL
+	, @docNumber nvarchar(10) = NULL
+	, @customerName nvarchar(70) = NULL
+	, @patientFullName nvarchar(150) = NULL
+	, @doctorFullName nvarchar(150) = NULL
+	, @createDateFrom datetime = NULL
+	, @createDateTo datetime = NULL
+	, @userId int = NULL	
+	, @jsonStringStatus nvarchar(max) = NULL
+)
+
+AS
+BEGIN 
+	
+	SET NOCOUNT ON;
+
+	DECLARE @employeePositionId int
+			
+	
+	CREATE TABLE #positionsId (EmployeePositionId int)
+
+	INSERT #positionsId
+	SELECT EmployeePositionID
+	FROM dbo.EmployeePositionsCombination
+	WHERE EmployeeID = @userId
+
+	IF (SELECT COUNT(1) FROM #positionsId) = 1 BEGIN
+
+		SELECT @employeePositionId = EmployeePositionId FROM #positionsId
+
+		IF @employeePositionId = 4
+		SELECT * 
+		FROM dbo.GetWorkOrdersList
+		(@branchTypeID, @branchType, @workorderID, @docNumber, @customerName	, @patientFullName, @doctorFullName, @createDateFrom	, @createDateTo, default	, @jsonStringStatus)
+		WHERE Status BETWEEN 9 AND 99
+			OR (BranchTypeID = 1 AND FlagStl = 1 AND Status = 0)
+
+		IF @employeePositionId = 2
+		SELECT * 
+		FROM dbo.GetWorkOrdersList
+		(@branchTypeID, @branchType, @workorderID, @docNumber, @customerName	, @patientFullName, @doctorFullName, @createDateFrom	, @createDateTo, default	, @jsonStringStatus)
+		WHERE BranchTypeID = 1 
+			AND FlagStl = 0
+
+		IF @employeePositionId = 3
+		SELECT * 
+		FROM dbo.GetWorkOrdersList
+		(@branchTypeID, @branchType, @workorderID, @docNumber, @customerName	, @patientFullName, @doctorFullName, @createDateFrom	, @createDateTo, default	, @jsonStringStatus)
+		
+		END
+
+	ELSE SELECT * 
+		FROM dbo.GetWorkOrdersList
+		(@branchTypeID, @branchType, @workorderID, @docNumber, @customerName	, @patientFullName, @doctorFullName, @createDateFrom	, @createDateTo, default	, @jsonStringStatus)
+
+	DROP TABLE #positionsId
+END
+GO
+GRANT EXECUTE
+    ON OBJECT::[dbo].[GetRelevantWorkOrdersList] TO [gdl_user]
+    AS [dbo];
+
